dc95d8ce7f95bfd0bd5d0cfb58d99d4a5713c5fb
do0b <dbrandl72@gmail.com>
2024-01-30 12:16:59 +0100

    changes for test deployment

--- cdk8s/main.ts
+++ cdk8s/main.ts
@@ -12,7 +12,7 @@ class MyChart extends Chart {
         namespace: ns
       },
       spec: {
-        replicas: 3,
+        replicas: 2,
         selector: { matchLabels: { app: appLabel } },
         template: {
           metadata: { labels: { app: appLabel } },
